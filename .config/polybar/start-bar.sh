4b26d49270a1b5af0d152be4af667896f3a1b011
KevinYakar <kevinyakar@gmail.com>
2021-04-04 03:01:29 -0700

    fix start-bar.sh for polybar; change dunst toggle to open signal

--- .config/polybar/start-bar.sh
+++ .config/polybar/start-bar.sh
@@ -2,4 +2,5 @@
 
 numMonitors=$(( $( xrandr --listactivemonitors | wc -l ) - 1 ))
 [ $numMonitors -eq 2 ] && polybar first & polybar second || polybar first &
+sleep 1
 ~/.config/polybar/toggle-tray.sh
